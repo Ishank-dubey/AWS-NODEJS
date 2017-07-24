0ee1faa0be5fef1abba1b42edf9f34d2ddc5a419
PS <PS@HECTOR>
2017-07-24 08:46:54 +0530

    Adding getObject S3 method

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /**
- * http://usejsdoc.org/
+ * Simple S3 bucket operations
  */
 var AWS = require('aws-sdk');
 
@@ -17,5 +17,14 @@ var params = {
 		  else    { 
 			  	console.log(data);
 			  	console.log('success');
+			  	params.Key = 'index.html';  	
+			  	getObject();
 		  }           // successful response
 		});
+		
+		function getObject(){
+		 s3.getObject(params, function(err, data) {
+			   if (err) console.log(err); // an error occurred
+			  else     console.log(data);           // successful response
+			 });		
+		 }
